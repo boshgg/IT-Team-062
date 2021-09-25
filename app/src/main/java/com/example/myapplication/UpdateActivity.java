ab0a7936df9bec08b14de8ca288621e9cd59cfdb
yuehaos <83277907+yuehaos@users.noreply.github.com>
2021-09-26 03:09:55 +0800

    Update UpdateActivity.java

--- app/src/main/java/com/example/myapplication/UpdateActivity.java
+++ app/src/main/java/com/example/myapplication/UpdateActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +14,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class UpdateActivity extends AppCompatActivity {
-    EditText name_input, birthday_input, age_input;
-    Button update_button;
-    String id, name, birthday, age;
+    EditText name_input, birthday_input, age_input, gender_input;
+    Button update_button, delete_button;
+    String id, name, birthday, age, gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +26,14 @@ public class UpdateActivity extends AppCompatActivity {
         name_input = findViewById(R.id.name_input2);
         age_input = findViewById(R.id.age_input2);
         birthday_input = findViewById(R.id.birthday_input2);
+        gender_input = findViewById(R.id.gender_input2);
         update_button = findViewById(R.id.add_button2);
+        delete_button = findViewById(R.id.delete_button);
         getAndSetIntentData();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(name);
+        }
         update_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,30 +42,63 @@ public class UpdateActivity extends AppCompatActivity {
                 name = name_input.getText().toString();
                 birthday = birthday_input.getText().toString();
                 age = age_input.getText().toString();
+                gender = gender_input.getText().toString();
 
-                myDB.updateData(id, name, birthday, age);
+                myDB.updateData(id, name, birthday, age, gender);
 
                 Intent intent = new Intent(UpdateActivity.this, CreateDBActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog();
             }
         });
     }
 
     void getAndSetIntentData() {
         if (getIntent().hasExtra("id") && getIntent().hasExtra("name") &&
-                getIntent().hasExtra("birthday") && getIntent().hasExtra("age")){
+                getIntent().hasExtra("birthday") && getIntent().hasExtra("age")
+                && getIntent().hasExtra("gender")){
             id = getIntent().getStringExtra("id");
             name = getIntent().getStringExtra("name");
             birthday = getIntent().getStringExtra("birthday");
             age = getIntent().getStringExtra("age");
+            gender = getIntent().getStringExtra("gender");
 
             name_input.setText(name);
-            birthday_input.setText(birthday);
             age_input.setText(age);
+            birthday_input.setText(birthday);
+            gender_input.setText(gender);
 
         }
         else {
             Toast.makeText(this,"No Data", Toast.LENGTH_SHORT).show();
         }
     }
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete " + name + " ?");
+        builder.setMessage("Are you sure to delete " + name + " ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                CustomerInfoDBHelper myDB = new CustomerInfoDBHelper(UpdateActivity.this);
+                myDB.deleteOneRow(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
+    }
+
+
 }
