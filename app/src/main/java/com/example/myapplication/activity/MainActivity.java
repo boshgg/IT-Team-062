8f604d4a0cae4522aa019a1a8e4d48f359ce5014
yuehaos <83277907+yuehaos@users.noreply.github.com>
2021-10-19 19:28:28 +0800

    Main activity to show log in interface

---
+++ app/src/main/java/com/example/myapplication/activity/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+import com.example.myapplication.db.UserService;
+
+public class MainActivity extends AppCompatActivity {
+    final UserService userService  = new UserService(this);
+    private Button mBtnLogin;
+    private EditText mEtUserName;
+    private EditText mEtPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnLogin = findViewById((R.id.btn_login));
+        mEtUserName = findViewById(R.id.userName);
+        mEtPassword = findViewById(R.id.password);
+        setListeners();
+
+    }
+
+
+    /***
+     * set listeners for all the buttons in this activity
+     */
+    private void setListeners(){
+        OnClick onClick = new OnClick();
+        mBtnLogin.setOnClickListener(onClick);
+    }
+
+    private class OnClick implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            if (view.getId() == R.id.btn_login){
+                String username =mEtUserName.getText().toString().trim();
+                String password =mEtPassword.getText().toString().trim();
+                if (username.isEmpty()){
+                    Toast.makeText(MainActivity.this, "username is empty", Toast.LENGTH_SHORT).show();
+                }else if (password.isEmpty()){
+                    Toast.makeText(MainActivity.this, "password is empty", Toast.LENGTH_SHORT).show();
+                }
+                // activity jumped after login
+                // waiting for changing activity
+                else if (userService.login(username,password)) {
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    intent = new Intent(MainActivity.this, CreateDBActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    }
+}
